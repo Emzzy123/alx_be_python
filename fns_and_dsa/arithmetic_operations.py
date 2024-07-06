a8afea7b2a84f9bb173f17fea4d245d3fd95012a
Ifeanyi Chidoka <ik.chidoka@gmail.com>
2024-07-06 14:47:13 +0100

    Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,19 @@
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        result = num1 + num2
+        return result
+    elif operation == 'subtract':
+        result = num1 - num2
+        return result
+    elif operation == 'multiply':
+        result = num1 * num2
+        return result
+    elif operation == 'divide':
+        if num2 == 0:
+            print("Error: Can't divide by zero(0)")
+        else:
+            result = num1 - num2
+            return result
+    else:
+        print("Error: Invalid operation.")
+
